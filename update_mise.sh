34f51cd8396256a88a5e1d1374cdc1d4d1f65193
Yuto Tanaka <karaage_nennen@icloud.com>
2025-02-03 21:54:08 +0900

    wip

--- update_mise.sh
+++ update_mise.sh
@@ -6,8 +6,6 @@
 
 readonly PRODUCT_VERSION="0.7.0"
 PRODUCT_NAME="$(basename "${0}")"
-readonly MISE_ZIG_BINDIR="$MISE_DATA_DIR/installs/zig"
-readonly BIN_ZIG="bin/zig"
 
 #=======================
 # 関数定義
@@ -15,7 +13,7 @@ readonly BIN_ZIG="bin/zig"
 
 # 使い方、ヘルプメッセージ
 usage() {
-    cat <<EOF
+  cat <<EOF
 $PRODUCT_NAME(bash)
 Update mise-tools
 
@@ -23,6 +21,10 @@ Usage:
     $PRODUCT_NAME <COMMAND> <SUBCOMMANDS>
 
 Commands:
+    neovim-master                  Run update mise neovim master
+    neovim-latest                  Run update mise neovim latest
+    neovim-stable                  Run update mise neovim stable
+    neovim-nightly                 Run update mise neovim nightly
     paleovim-master                Run update mise paleovim master
     paleovim-latest                Run update mise paleovim latest
     zig-master                     Run update mise zig master
@@ -39,136 +41,60 @@ EOF
 
 # バージョン情報出力
 version() {
-    echo "$PRODUCT_NAME(bash) v$PRODUCT_VERSION"
-}
-
-function _paleovim_master() {
-    local commit_hash_file
-    commit_hash_file="$HOME/.cache/paleovim-master-commit-hash.txt"
-    local commit_hash
-    commit_hash=$(cat "$commit_hash_file")
-    local new_commit_hash
-    new_commit_hash=$(git ls-remote --heads --tags https://github.com/vim/vim.git | grep refs/heads/master | cut -f 1)
-
-    if [ "$commit_hash" != "$new_commit_hash" ]; then
-        echo "paleovim (latest)master found!"
-        echo "$new_commit_hash" >"$commit_hash_file"
-        if [ "$opt" == "--use-pueue" ]; then
-            task_id=$(pueue add -p -- "mise uninstall vim@ref:master")
-            pueue add --after "$task_id" -- "mise install vim@ref:master"
-        else
-            mise uninstall vim@ref:master
-            mise install vim@ref:master
-        fi
-    else
-        echo "paleovim (latest)master is already installed"
-        echo "commit hash: $commit_hash"
-    fi
-}
-
-function _paleovim_latest() {
-    local version
-    version=$(mise current vim)
-    local new_version
-    new_version=$(mise latest vim)
-
-    if [ "$version" != "$new_version" ]; then
-        echo "paleovim latest version ( $new_version) found!"
-        if [ "$opt" == "--use-pueue" ]; then
-            task_id=$(pueue add -p -- "mise uninstall vim@latest")
-            pueue add --after "$task_id" -- "mise install vim@latest"
-        else
-            mise uninstall "vim@latest"
-            mise install "vim@latest"
-        fi
-    else
-        echo "paleovim latest version ( $new_version ) is already installed"
-    fi
-}
-
-function _zig_master() {
-    local zig_version_file
-    zig_version_file="$HOME/.cache/zig-master-version.txt"
-    local zig_version
-    zig_version=$(cat "$zig_version_file")
-    local new_zig_version
-    new_zig_version=$(curl -sSL https://ziglang.org/download/index.json | jq .master.version --raw-output)
-
-    if [ "$zig_version" != "$new_zig_version" ]; then
-        echo "zig (latest)master found!"
-        echo "$new_zig_version" >"$zig_version_file"
-        if [ "$opt" == "--use-pueue" ]; then
-            task_id=$(pueue add -p -- "mise uninstall zig@ref:master")
-            pueue add --after "$task_id" -- "mise install zig@ref:master"
-        else
-            mise uninstall zig@ref:master
-            mise install zig@ref:master
-        fi
-    else
-        echo "zig (latest)master is already installed"
-        echo "version: $zig_version"
-    fi
-}
-
-function _zig_latest() {
-    local version
-    version=$("$MISE_ZIG_BINDIR/latest/$BIN_ZIG" version)
-    local new_version
-    new_version=$(mise latest zig)
-
-    if [ "$version" != "$new_version" ]; then
-        echo "zig (latest)master found!"
-        if [ "$opt" == "--use-pueue" ]; then
-            task_id=$(pueue add -p -- "mise uninstall zig@$version")
-            pueue add --after "$task_id" -- "mise install zig@$new_version"
-        else
-            mise uninstall "zig@$version"
-            mise install "zig@$new_version"
-        fi
-    else
-        echo "zig (latest)master ( $version ) is already installed"
-    fi
+  echo "$PRODUCT_NAME(bash) v$PRODUCT_VERSION"
 }
 
 #=======================
 # メイン処理
 #=======================
 while (("$#")); do
-    case "$1" in
-    -h | --help | help)
-        usage
-        exit 1
-        ;;
-    -v | --version | version)
-        version
-        exit 1
-        ;;
-    paleovim-master | paleovim-latest | zig-master | zig-latest)
-        cmd=$1
-        opt=$2
-        shift
-        ;;
-    *)
-        break
-        ;;
-    esac
-done
-
-case $cmd in
-paleovim-master)
-    _paleovim_master "$opt"
-    ;;
-paleovim-latest)
-    _paleovim_latest "$opt"
-    ;;
-zig-master)
-    _zig_master "$opt"
-    ;;
-zig-latest)
-    _zig_latest "$opt"
-    ;;
-*)
+  case "$1" in
+  -h | --help | help)
     usage
     exit 1
     ;;
+  -v | --version | version)
+    version
+    exit 1
+    ;;
+  neovim-master | neovim-latest | neovim-stable | neovim-nightly | paleovim-master | paleovim-latest | zig-master | zig-latest)
+    cmd=$1
+    opt=$2
+    shift
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
+case $cmd in
+neovim-master)
+  echo "WIP"
+  ;;
+neovim-latest)
+  echo "WIP"
+  ;;
+neovim-stable)
+  echo "WIP"
+  ;;
+neovim-nightly)
+  echo "WIP"
+  ;;
+paleovim-master)
+  update_mise_paleovim_master "$opt"
+  ;;
+paleovim-latest)
+  update_mise_paleovim_latest "$opt"
+  ;;
+zig-master)
+  update_mise_zig_master "$opt"
+  ;;
+zig-latest)
+  update_mise_zig_latest "$opt"
+  ;;
+*)
+  usage
+  exit 1
+  ;;
 esac
